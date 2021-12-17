ada99b7e0fa36723fae14a388c4065cb8e93c957
motionwebdevs <motionwebteam@gmail.com>
2021-12-17 16:19:15 +0600

    responsive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 $(".slider").slick({
     slidesToShow: 3,
+    responsive: [
+        {
+            breakpoint: 769,
+            settings: {
+                slidesToShow: 2,
+                arrows: false,
+                autoplay: true
+
+            }
+        },
+        {
+            breakpoint: 541,
+            settings: {
+                arrows: false,
+
+                slidesToShow: 1,
+
+
+            }
+        },
+    ]
 
 })
